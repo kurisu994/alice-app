6fdd7b18537a769cfd19ea539eb45caf8ccb0e58
kurisu <makise_kurisuu@outlook.jp>
2019-01-29 16:44:54 +0800

    解决键盘弹出遮挡输入框

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -12,8 +12,29 @@ class LoginPage extends StatelessWidget {
           appBar: AppBar(
             title: Text('登录页'),
           ),
-          body: new Center(
-            child: new LoginHomePage(),
+          body: new SafeArea(
+
+            ///解决键盘弹出遮挡输入框的解决方案
+            child: new SingleChildScrollView(
+              child: new Container(
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                decoration: new BoxDecoration(
+                    gradient: new LinearGradient(
+                      colors: const [Color(0xFFfbab66), Color(0xFFf7418c)],
+                      stops: const [0.0, 1.0],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    )),
+                child: new LoginHomePage(),
+              ),
+            ),
           ),
         ));
   }
@@ -29,12 +50,20 @@ class LoginHomePageState extends State<LoginHomePage> {
   final _userNameTextController = TextEditingController();
   final _passwordTextController = TextEditingController();
   bool _showLoading = false;
+  bool showPassword = true;
 
-  void _submit() => {};
+  void _submit() {}
+
+  void _showPassword() {
+    setState(() {
+      showPassword = !showPassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      alignment: Alignment.center,
       children: <Widget>[
         new Container(
           padding: EdgeInsets.all(10.0),
@@ -45,30 +74,47 @@ class LoginHomePageState extends State<LoginHomePage> {
               children: <Widget>[
                 Image.asset(
                   'assets/images/amadeus.png',
-                  height: 180.0,
+                  height: 165.0,
                   fit: BoxFit.fitHeight,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 50.0),
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 50.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: '用户名',
+                        icon: new Icon(Icons.supervisor_account),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return '请输入用户名';
+                        }
+                      },
+                      controller: _userNameTextController,
+                    ),
+                  ),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: '用户名'),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return '请输入用户名';
-                    }
-                  },
-                  controller: _userNameTextController,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: '密码'),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return '请输入密码';
-                    }
-                  },
-                  controller: _passwordTextController,
-                  obscureText: true,
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 5.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: '密码',
+                          icon: new Icon(Icons.lock),
+                          suffixIcon: new IconButton(
+                            icon: new Icon(Icons.remove_red_eye,
+                                color: Colors.black),
+                            onPressed: _showPassword,
+                          )),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return '请输入密码';
+                        }
+                      },
+                      controller: _passwordTextController,
+                      obscureText: !showPassword,
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 20.0),
